f762258da6cb171b338116a5c905bcb9639b9af4
Glenn Sarti <glenn.sarti@outlook.com>
2015-02-08 16:50:58 +0800

    Modified package to use the Spoon JRE image instead.  No need for me to generate yet another JRE installer

--- src/neo4j-community-2.1.6/Package-Definition.ps1
+++ src/neo4j-community-2.1.6/Package-Definition.ps1
@@ -1,6 +1,5 @@
 $PackageDownloads = @{
   "neo4j-community-2.1.6-windows.zip" = "http://neo4j.com/artifact.php?name=neo4j-community-2.1.6-windows.zip";
-  "jre-7u75-windows-x64.exe" = "http://javadl.sun.com/webapps/download/AutoDL?BundleId=101469";
 }
 $PackageName = 'neo4j-community'
 $PackageVersion = '2.1.6'
