76380eef0d7058973b46b79164f66e3840fd12a7
Glenn Sarti <glenn.sarti@outlook.com>
2015-03-04 19:48:19 +0800

    Added 2.2.0-M04 image

---
+++ src/neo4j-community-2.2.0-M04/Package-Definition.ps1
@@ -0,0 +1,6 @@
+$PackageDownloads = @{
+  "neo4j-community-2.2.0-M04-windows.zip" = "http://neo4j.com/artifact.php?name=neo4j-community-2.2.0-M04-windows.zip";
+}
+$PackageName = 'neo4j-community'
+$PackageVersion = '2.2.0-M04'
+
