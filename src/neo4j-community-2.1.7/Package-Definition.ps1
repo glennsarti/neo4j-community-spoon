513329a5c38bce5ba843fa71b131cfa3ece468df
Glenn Sarti <glenn.sarti@outlook.com>
2015-03-04 19:48:09 +0800

    Added 2.1.7 Neo4j image

---
+++ src/neo4j-community-2.1.7/Package-Definition.ps1
@@ -0,0 +1,6 @@
+$PackageDownloads = @{
+  "neo4j-community-2.1.7-windows.zip" = "http://neo4j.com/artifact.php?name=neo4j-community-2.1.7-windows.zip";
+}
+$PackageName = 'neo4j-community'
+$PackageVersion = '2.1.7'
+
